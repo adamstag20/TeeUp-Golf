8332d6da2f0fae6b8a9f0a0f6d6a7ea2a5f42ced
Adam Stagner <stagner2@pdx.edu>
2023-11-26 15:32:35 -0800

    Added teeTime data structure, no bookings currently in database

--- backend/course/admin.py
+++ backend/course/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 
-from .models import Course
+from .models import Course, TeeTime
 
 class CourseAdmin(admin.ModelAdmin):
     list_display = ('name','holes','strokes','diff','description','driving_range','food','address','phone')
 
+class TeeTimeAdmin(admin.ModelAdmin):
+
+    list_display= ('course','owner','date','time','players')
+
 # Register your models here.
 
 admin.site.register(Course, CourseAdmin)
+admin.site.register(TeeTime, TeeTimeAdmin)
--- backend/course/models.py
+++ backend/course/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
 class Course(models.Model):
 
@@ -15,3 +15,10 @@ class Course(models.Model):
 
     def _str_(self):
         return self.name
+
+class TeeTime(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    owner = models.CharField(max_length=120, default='')
+    date = models.DateField()
+    time = models.TimeField()
+    players = models.IntegerField(default=1)
